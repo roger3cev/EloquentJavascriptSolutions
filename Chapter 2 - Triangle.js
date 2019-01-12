357fd483a429c4f6090f70e1666e1c07d14989f3
RogerWillis <roger.willis@r3cev.com>
2019-01-12 10:06:37 +0000

    Fixed typos.

--- Chapter 2 - Triangle.js
+++ Chapter 2 - Triangle.js
@@ -3,10 +3,10 @@
 "use strict"
 
 /**
- * Array Extension function to obtain last element in an array.
+ * Array extension function to obtain the last element in an array.
  * If the array is empty the function returns "undefined".
  * 
- * @returns {any} The last element in the array.
+ * @returns {any} The last element in the array or undefined if the array is empty
  */
 Array.prototype.last = function () {
   return this[this.length - 1]
@@ -14,7 +14,7 @@ Array.prototype.last = function () {
 
 /**
  * Prints a triangle of the specified size using the specified character. If no
- * depth is provided then the functino will throw an exception.
+ * depth is provided then the function will throw an exception.
  * 
  * @param {String} char - The character to print (defaults to "#")
  * @param {Number} depth - The height of the triangle (in lines)
@@ -22,7 +22,7 @@ Array.prototype.last = function () {
  */
 const triangle = function (char = "#", depth) {
   if (depth === undefined) throw new Error("Depth must be defined.")
-  // (Tail) recursively builds an array of strings represeting the triangle.
+  // (Tail) recursively builds an array of strings representing the triangle.
   const buildTriangle = (rows = []) => {
     if (rows.length === depth) {
       return rows
